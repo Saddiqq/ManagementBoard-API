84b81bc3d369f87711e1fb9c9650786520a19b8b
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 13:49:42 +0400

    Added id to Repository

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Repository/CardRepository.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Repository/CardRepository.java
@@ -4,7 +4,11 @@ package com.example.Management.Board.API.Repository;
 import com.example.Management.Board.API.Model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findByBoardId(Long boardId);
+    Optional<Card> findByIdAndBoardId(Long id, Long boardId);
 }
