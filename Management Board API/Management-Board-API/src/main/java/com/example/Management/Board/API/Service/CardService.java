30a34bc4b692e2965ef25808d647f1aa560913fe
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 15:11:09 +0400

    updated the CardService.java

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/CardService.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/CardService.java
@@ -1,6 +1,7 @@
 package com.example.Management.Board.API.Service;
 
 import com.example.Management.Board.API.Model.Card;
+import com.example.Management.Board.API.Model.Board;
 import com.example.Management.Board.API.Repository.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,20 +18,21 @@ public class CardService {
         this.cardRepository = cardRepository;
     }
 
-    public Card createCard(Card card) {
+    public Card createCard(Board board, Card card) {
+        card.setBoard(board);
         return cardRepository.save(card);
     }
 
-    public List<Card> getAllCards(Long boardId) {
-        return cardRepository.findByBoardId(boardId);
+    public List<Card> getAllCards(Board board) {
+        return cardRepository.findByBoard(board);
     }
 
-    public Card getCardById(Long boardId, Long cardId) {
-        return cardRepository.findByIdAndBoardId(cardId, boardId).orElse(null);
+    public Card getCardById(Board board, Long cardId) {
+        return cardRepository.findByIdAndBoard(cardId, board).orElse(null);
     }
 
-    public Card updateCard(Long boardId, Long cardId, Card cardDetails) {
-        Card card = cardRepository.findByIdAndBoardId(cardId, boardId).orElse(null);
+    public Card updateCard(Board board, Long cardId, Card cardDetails) {
+        Card card = cardRepository.findByIdAndBoard(cardId, board).orElse(null);
         if (card != null) {
             card.setTitle(cardDetails.getTitle());
             card.setDescription(cardDetails.getDescription());
@@ -40,11 +42,11 @@ public class CardService {
         return null;
     }
 
-    public String deleteCard(Long boardId, Long cardId) {
-        Card card = cardRepository.findByIdAndBoardId(cardId, boardId).orElse(null);
+    public String deleteCard(Board board, Long cardId) {
+        Card card = cardRepository.findByIdAndBoard(cardId, board).orElse(null);
         if (card != null) {
             cardRepository.delete(card);
-            return "Card with ID " + cardId + " has been deleted successfully from board " + boardId + ".";
+            return "Card with ID " + cardId + " has been deleted successfully from board " + board.getId() + ".";
         }
         return null;
     }
