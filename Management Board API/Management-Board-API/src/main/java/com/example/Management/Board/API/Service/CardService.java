73126ad466557e97b00deab8c98d8448b3721646
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 13:47:49 +0400

    added methods to CardService.java

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/CardService.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/CardService.java
@@ -1,15 +1,51 @@
 package com.example.Management.Board.API.Service;
 
+import com.example.Management.Board.API.Model.Card;
 import com.example.Management.Board.API.Repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CardService {
+
     private final CardRepository cardRepository;
 
+    @Autowired
     public CardService(CardRepository cardRepository) {
         this.cardRepository = cardRepository;
     }
 
-    // Here you can add your methods for create, read, update, and delete
+    public Card createCard(Long boardId, Card card) {
+        return cardRepository.save(card);
+    }
+
+    public List<Card> getAllCards(Long boardId) {
+        return cardRepository.findByBoardId(boardId);
+    }
+
+    public Card getCardById(Long boardId, Long cardId) {
+        return cardRepository.findByIdAndBoardId(cardId, boardId).orElse(null);
+    }
+
+    public Card updateCard(Long boardId, Long cardId, Card cardDetails) {
+        Card card = cardRepository.findByIdAndBoardId(cardId, boardId).orElse(null);
+        if (card != null) {
+            card.setTitle(cardDetails.getTitle());
+            card.setDescription(cardDetails.getDescription());
+            card.setSection(cardDetails.getSection());
+            return cardRepository.save(card);
+        }
+        return null;
+    }
+
+    public String deleteCard(Long boardId, Long cardId) {
+        Card card = cardRepository.findByIdAndBoardId(cardId, boardId).orElse(null);
+        if (card != null) {
+            cardRepository.delete(card);
+            return "Card with ID " + cardId + " has been deleted successfully from board " + boardId + ".";
+        }
+        return null;
+    }
 }
