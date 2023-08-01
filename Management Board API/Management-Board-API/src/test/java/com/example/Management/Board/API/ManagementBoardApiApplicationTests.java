bdc6f51c92f0a3644b8bcc919322e65232c6731a
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 10:52:17 +0400

    Setting up

---
+++ Management Board API/Management-Board-API/src/test/java/com/example/Management/Board/API/ManagementBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Management.Board.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
