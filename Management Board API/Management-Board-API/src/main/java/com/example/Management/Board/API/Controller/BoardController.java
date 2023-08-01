d4c295936ff3f2173c9357fa1eddec3d9cb9c78c
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 12:15:21 +0400

    Created BoardController.java and modified BoardService.java

---
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Controller/BoardController.java
@@ -0,0 +1,52 @@
+package com.example.Management.Board.API.Controller;
+
+import com.example.Management.Board.API.Model.Board;
+import com.example.Management.Board.API.Service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/boards")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @Autowired
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Board> createBoard(@RequestBody Board board) {
+        Board newBoard = boardService.createBoard(board);
+        return new ResponseEntity<>(newBoard, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Board>> getAllBoards() {
+        List<Board> boards = boardService.getAllBoards();
+        return new ResponseEntity<>(boards, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Board> getBoardById(@PathVariable("id") Long id) {
+        Board board = boardService.getBoardById(id);
+        return new ResponseEntity<>(board, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Board> updateBoard(@PathVariable("id") Long id, @RequestBody Board board) {
+        Board updatedBoard = boardService.updateBoard(id, board);
+        return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBoard(@PathVariable("id") Long id) {
+        boardService.deleteBoard(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/BoardService.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/BoardService.java
@@ -1,16 +1,43 @@
 package com.example.Management.Board.API.Service;
 
-import org.springframework.stereotype.Service;
+import com.example.Management.Board.API.Model.Board;
 import com.example.Management.Board.API.Repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BoardService {
+
     private final BoardRepository boardRepository;
 
+    @Autowired
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
 
-    // Here you can add your methods for create, read, update, and delete
-}
+    public Board createBoard(Board board) {
+        // Your implementation goes here
+        return null;
+    }
 
+    public List<Board> getAllBoards() {
+        // Your implementation goes here
+        return null;
+    }
+
+    public Board getBoardById(Long id) {
+        // Your implementation goes here
+        return null;
+    }
+
+    public Board updateBoard(Long id, Board board) {
+        // Your implementation goes here
+        return null;
+    }
+
+    public void deleteBoard(Long id) {
+        // Your implementation goes here
+    }
+}
