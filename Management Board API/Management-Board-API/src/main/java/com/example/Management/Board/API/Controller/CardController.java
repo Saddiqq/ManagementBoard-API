9e329d68c527c7554eca2d30310f80b0fc245f30
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 15:10:55 +0400

    updated the CardController

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Controller/CardController.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Controller/CardController.java
@@ -1,9 +1,9 @@
 package com.example.Management.Board.API.Controller;
 
-import com.example.Management.Board.API.Model.Board;
 import com.example.Management.Board.API.Model.Card;
-import com.example.Management.Board.API.Service.BoardService;
+import com.example.Management.Board.API.Model.Board;
 import com.example.Management.Board.API.Service.CardService;
+import com.example.Management.Board.API.Service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,35 +27,45 @@ public class CardController {
     @PostMapping
     public ResponseEntity<Card> createCard(@PathVariable("boardId") Long boardId, @RequestBody Card card) {
         Board board = boardService.getBoardById(boardId);
-        if (board == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        card.setBoard(board);
-        Card newCard = cardService.createCard(card);
+        if(board == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        Card newCard = cardService.createCard(board, card);
         return new ResponseEntity<>(newCard, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Card>> getAllCards(@PathVariable("boardId") Long boardId) {
-        List<Card> cards = cardService.getAllCards(boardId);
+        Board board = boardService.getBoardById(boardId);
+        if(board == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        List<Card> cards = cardService.getAllCards(board);
         return new ResponseEntity<>(cards, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Card> getCardById(@PathVariable("boardId") Long boardId, @PathVariable("id") Long id) {
-        Card card = cardService.getCardById(boardId, id);
+        Board board = boardService.getBoardById(boardId);
+        if(board == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        Card card = cardService.getCardById(board, id);
         return new ResponseEntity<>(card, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Card> updateCard(@PathVariable("boardId") Long boardId, @PathVariable("id") Long id, @RequestBody Card card) {
-        Card updatedCard = cardService.updateCard(boardId, id, card);
+        Board board = boardService.getBoardById(boardId);
+        if(board == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        Card updatedCard = cardService.updateCard(board, id, card);
         return new ResponseEntity<>(updatedCard, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCard(@PathVariable("boardId") Long boardId, @PathVariable("id") Long id) {
-        String response = cardService.deleteCard(boardId, id);
+        Board board = boardService.getBoardById(boardId);
+        if(board == null)
+            return new ResponseEntity<>("Board not found", HttpStatus.NOT_FOUND);
+        String response = cardService.deleteCard(board, id);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
