6ed57ad5afc511a3f615e14242a5c7a15991e311
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 13:50:42 +0400

    Modified / created new variables in Board.java

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Model/Board.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Model/Board.java
@@ -1,18 +1,49 @@
 package com.example.Management.Board.API.Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-
+import jakarta.persistence.*;
+import java.util.Map;
 
 @Entity
-@Table(name = "boards")
-public class Board extends BaseEntity {
+public class Board {
 
-    @Column(name = "name")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
+    @ElementCollection
+    private Map<Integer, String> columns;
 
-    // getters and setters...
+    public Board() {
+        // Default constructor
+    }
+
+    public Board(String name, Map<Integer, String> columns) {
+        this.name = name;
+        this.columns = columns;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<Integer, String> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Map<Integer, String> columns) {
+        this.columns = columns;
+    }
 }
 
