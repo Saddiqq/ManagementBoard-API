50908a240cac41fd9ef01de2710c426c0da238cc
Saddiqq <9adi8.tm@gmail.com>
2023-08-01 10:58:49 +0400

    Added BaseEntity.java

---
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Model/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.example.Management.Board.API.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    // getters and setters...
+}
+
