6e1a010e8888ddeceb20b1ff31c62c94b9610896
Saddiqq <9adi8.tm@gmail.com>
2023-08-02 15:38:41 +0400

    Uodated CardRepository.java to handle board IDs

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Repository/CardRepository.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Repository/CardRepository.java
@@ -1,8 +1,9 @@
 package com.example.Management.Board.API.Repository;
 
-import com.example.Management.Board.API.Model.Board;
 import com.example.Management.Board.API.Model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,10 @@ import java.util.Optional;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
-    List<Card> findByBoard(Board board);
-    Optional<Card> findByIdAndBoard(Long id, Board board);
+
+    @Query("SELECT c FROM Card c WHERE c.board.id = :boardId")
+    List<Card> findByBoardId(@Param("boardId") Long boardId);
+
+    @Query("SELECT c FROM Card c WHERE c.id = :id AND c.board.id = :boardId")
+    Optional<Card> findByIdAndBoardId(@Param("id") Long id, @Param("boardId") Long boardId);
 }
--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/CardService.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/CardService.java
@@ -1,45 +1,51 @@
 package com.example.Management.Board.API.Service;
 
+import com.example.Management.Board.API.Model.Board;
 import com.example.Management.Board.API.Model.Card;
+import com.example.Management.Board.API.Repository.BoardRepository;
 import com.example.Management.Board.API.Repository.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 public class CardService {
 
     private final CardRepository cardRepository;
+    private final BoardRepository boardRepository;
 
     @Autowired
-    public CardService(CardRepository cardRepository) {
+    public CardService(CardRepository cardRepository, BoardRepository boardRepository) {
         this.cardRepository = cardRepository;
+        this.boardRepository = boardRepository;
     }
 
-    public Card addCard(Card card) {
+    public Card addCard(Long boardId, Card card) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new NoSuchElementException("Board with id " + boardId + " not found"));
+        card.setBoard(board);
         return cardRepository.save(card);
     }
 
-    public List<Card> findAllCards() {
-        return cardRepository.findAll();
+    public List<Card> findAllCards(Long boardId) {
+        return cardRepository.findByBoardId(boardId);
     }
 
-    public Card updateCard(Card card) {
+    public Card updateCard(Long boardId, Card card) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new NoSuchElementException("Board with id " + boardId + " not found"));
+        card.setBoard(board);
         return cardRepository.save(card);
     }
 
-    public Card findCardById(Long id) {
-        return cardRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Card with id " + id + " not found"));
+    public Card findCardById(Long boardId, Long id) {
+        return cardRepository.findByIdAndBoardId(id, boardId)
+                .orElseThrow(() -> new NoSuchElementException("Card with id " + id + " not found in board " + boardId));
     }
 
-    public void deleteCard(Long id) {
-        if (!cardRepository.existsById(id)) {
-            throw new NoSuchElementException("Card with id " + id + " not found");
-        }
-        cardRepository.deleteById(id);
+    public void deleteCard(Long boardId, Long id) {
+        cardRepository.findByIdAndBoardId(id, boardId).ifPresent(cardRepository::delete);
     }
 }
