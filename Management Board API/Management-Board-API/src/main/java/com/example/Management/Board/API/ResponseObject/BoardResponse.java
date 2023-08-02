320e53001200f261320f9eaef27c5c51f8f3b43a
Saddiqq <9adi8.tm@gmail.com>
2023-08-02 13:31:58 +0400

    Created a BoardResponse.java Class to handle responses

---
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/ResponseObject/BoardResponse.java
@@ -0,0 +1,14 @@
+package com.example.Management.Board.API.ResponseObject;
+
+
+import java.util.Map;
+
+public class BoardResponse {
+
+    private Long boardId;
+    private String name;
+    private Map<Integer, String> columns;
+
+    // getters and setters
+}
+
