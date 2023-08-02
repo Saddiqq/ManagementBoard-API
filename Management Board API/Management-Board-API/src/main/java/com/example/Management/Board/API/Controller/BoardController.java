04073430408bae92ff4c5994182644c61dc2b3b8
Saddiqq <9adi8.tm@gmail.com>
2023-08-02 13:40:55 +0400

    updated BoardController.java to use BoardResponse

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Controller/BoardController.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.Management.Board.API.Controller;
 
 import com.example.Management.Board.API.Model.Board;
+import com.example.Management.Board.API.ResponseObject.BoardResponse;
 import com.example.Management.Board.API.Service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,27 +22,33 @@ public class BoardController {
     }
 
     @PostMapping
-    public ResponseEntity<Board> createBoard(@RequestBody Board board) {
-        Board newBoard = boardService.createBoard(board);
+    public ResponseEntity<BoardResponse> createBoard(@RequestBody Board board) {
+        BoardResponse newBoard = boardService.createBoard(board);
         return new ResponseEntity<>(newBoard, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Board>> getAllBoards() {
-        List<Board> boards = boardService.getAllBoards();
+    public ResponseEntity<List<BoardResponse>> getAllBoards() {
+        List<BoardResponse> boards = boardService.getAllBoards();
         return new ResponseEntity<>(boards, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Board> getBoardById(@PathVariable("id") Long id) {
-        Board board = boardService.getBoardById(id);
-        return new ResponseEntity<>(board, HttpStatus.OK);
+    public ResponseEntity<BoardResponse> getBoardById(@PathVariable("id") Long id) {
+        BoardResponse board = boardService.getBoardById(id);
+        if (board != null) {
+            return new ResponseEntity<>(board, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Board> updateBoard(@PathVariable("id") Long id, @RequestBody Board board) {
-        Board updatedBoard = boardService.updateBoard(id, board);
-        return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
+    public ResponseEntity<BoardResponse> updateBoard(@PathVariable("id") Long id, @RequestBody Board board) {
+        BoardResponse updatedBoard = boardService.updateBoard(id, board);
+        if (updatedBoard != null) {
+            return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{id}")
