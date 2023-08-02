318d7130d1029ee91886d1aa4c216758ef33b3c0
Saddiqq <9adi8.tm@gmail.com>
2023-08-02 13:37:21 +0400

    implemented createBoardResponse method in BoardService.java

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/BoardService.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/Service/BoardService.java
@@ -1,12 +1,16 @@
 package com.example.Management.Board.API.Service;
 
 import com.example.Management.Board.API.Model.Board;
+import com.example.Management.Board.API.ResponseObject.BoardResponse;
 import com.example.Management.Board.API.Repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Date;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class BoardService {
@@ -18,19 +22,27 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
-    public Board createBoard(Board board) {
-        return boardRepository.save(board);
+    public BoardResponse createBoard(Board board) {
+        Board savedBoard = boardRepository.save(board);
+        return convertToBoardResponse(savedBoard);
     }
 
-    public List<Board> getAllBoards() {
-        return boardRepository.findAll();
+    public List<BoardResponse> getAllBoards() {
+        List<Board> boards = boardRepository.findAll();
+        return boards.stream()
+                .map(this::convertToBoardResponse)
+                .collect(Collectors.toList());
     }
 
-    public Board getBoardById(Long id) {
-        return boardRepository.findById(id).orElse(null);
+    public BoardResponse getBoardById(Long id) {
+        Board board = boardRepository.findById(id).orElse(null);
+        if(board != null) {
+            return convertToBoardResponse(board);
+        }
+        return null;
     }
 
-    public Board updateBoard(Long id, Board boardDetails) {
+    public BoardResponse updateBoard(Long id, Board boardDetails) {
         Board board = boardRepository.findById(id).orElse(null);
         if (board != null) {
             board.setTitle(boardDetails.getTitle());
@@ -40,12 +52,27 @@ public class BoardService {
                 board.setColumns(boardDetails.getColumns());
             }
 
-            return boardRepository.save(board);
+            Board updatedBoard = boardRepository.save(board);
+            return convertToBoardResponse(updatedBoard);
         }
         return null;
     }
+
     public String deleteBoard(Long id) {
         boardRepository.deleteById(id);
         return "Board with ID " + id + " has been deleted successfully.";
+    }
+
+    // Conversion method
+    private BoardResponse convertToBoardResponse(Board board) {
+        BoardResponse response = new BoardResponse();
+        response.setBoardId(board.getId());
+        response.setName(board.getTitle());
+
+        Map<Integer, String> columns = new HashMap<>();
+        // TODO: fill in the columns map based on the board entity
+        response.setColumns(columns);
+
+        return response;
     }
 }
