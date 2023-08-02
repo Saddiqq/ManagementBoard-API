cf17e4c1b9229cf779b1812b596e4b0f91484672
Saddiqq <9adi8.tm@gmail.com>
2023-08-02 14:22:28 +0400

    added the @EnableJpaAuditing annotation to ManagementBoardApiApplication.java

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/ManagementBoardApiApplication.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/ManagementBoardApiApplication.java
@@ -2,12 +2,13 @@ package com.example.Management.Board.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // Import this annotation
 
 @SpringBootApplication
+@EnableJpaAuditing // Add this annotation to enable JPA Auditing
 public class ManagementBoardApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementBoardApiApplication.class, args);
 	}
-
 }
