2334ada56ae104aac47a437f82582daa75c90dc9
Saddiqq <9adi8.tm@gmail.com>
2023-08-03 13:30:21 +0400

    Updated URL to fetch cards to use board.boardId instead of board.id.

--- Management Board API/Management-Board-API/src/main/resources/static/script.js
+++ Management Board API/Management-Board-API/src/main/resources/static/script.js
@@ -8,23 +8,23 @@ async function fetchBoardsAndCards() {
     }
 
     const boards = await response.json();
-    console.log(boards);  // Add this line to log the fetched boards
 
     for (const board of boards) {
-        if (!board.id) {
-            console.error(`Board with name ${board.name} has no id.`);
+        if (!board.boardId) {
+            console.error(`Board with title ${board.title} has no id.`);
             continue;
         }
 
         const boardDiv = document.createElement('div');
         boardDiv.className = 'board';
-        boardDiv.innerHTML = `<h2>${board.name}</h2>`;
+        boardDiv.innerHTML = `<h2>${board.title}</h2>`;
         appDiv.appendChild(boardDiv);
 
-        const cardResponse = await fetch(`http://localhost:8080/api/boards/${board.id}/cards`);
+        // Adjusted the url to fetch cards based on your data structure
+        const cardResponse = await fetch(`http://localhost:8080/api/boards/${board.boardId}/cards`);
 
         if (!cardResponse.ok) {
-            console.error(`Error fetching cards for board ${board.id}: ${cardResponse.statusText}`);
+            console.error(`Error fetching cards for board ${board.boardId}: ${cardResponse.statusText}`);
             continue;
         }
 
