b2b5ea4ae5cea53cc992775b721b602e5b64908a
Saddiqq <9adi8.tm@gmail.com>
2023-08-03 14:17:15 +0400

    Added Cross-Origin Resource Sharing

--- Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/ManagementBoardApiApplication.java
+++ Management Board API/Management-Board-API/src/main/java/com/example/Management/Board/API/ManagementBoardApiApplication.java
@@ -3,8 +3,10 @@ package com.example.Management.Board.API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // Import this annotation
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 @EnableJpaAuditing // Add this annotation to enable JPA Auditing
 public class 		ManagementBoardApiApplication {
 
