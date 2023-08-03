c47c076dcb0e7aea1cd129deb6e0ead07aef7a3f
Saddiqq <9adi8.tm@gmail.com>
2023-08-03 17:23:19 +0400

    recreated buttons for each card

--- Management Board API/Management-Board-API/src/main/resources/static/script.js
+++ Management Board API/Management-Board-API/src/main/resources/static/script.js
@@ -1,15 +1,94 @@
+async function fetchData(url) {
+    console.log(`Fetching data from ${url}`);
+    const response = await fetch(url, { method: 'GET' });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+async function putData(url, body) {
+    const response = await fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
+async function postData(url, body) {
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
+async function deleteData(url) {
+    const response = await fetch(url, { method: 'DELETE' });
+    const data = await response.json();
+    return data;
+}
+
+function createButton(name, callback) {
+    const button = document.createElement('button');
+    button.textContent = name;
+    button.addEventListener('click', callback);
+    return button;
+}
+
+function createBoardDiv(board) {
+    const div = document.createElement('div');
+    div.className = 'board';
+    div.textContent = `${board.title} - Columns: ${JSON.stringify(board.columns)}`;
+    return div;
+}
+
+function createCardDiv(boardId, card) {
+    const div = document.createElement('div');
+    div.className = 'card';
+    div.textContent = `${card.title} - Section: ${card.section} - Description: ${card.description}`;
+
+    const editButton = createButton('Edit', () => {
+        // TODO: call function to edit this card
+    });
+
+    const deleteButton = createButton('Delete', () => {
+        // TODO: call function to delete this card
+    });
+
+    const moveButton = createButton('Move', () => {
+        // TODO: call function to move this card
+    });
+
+    div.appendChild(editButton);
+    div.appendChild(deleteButton);
+    div.appendChild(moveButton);
+
+    return div;
+}
+
 async function fetchBoardsAndCards() {
+    console.log("Starting to fetch boards and cards.");
     const appDiv = document.getElementById('app');
 
     try {
-        console.log("Starting to fetch boards and cards.");
         const boards = await fetchData('http://localhost:8080/api/boards');
 
         for (const board of boards) {
             const boardDiv = createBoardDiv(board);
 
             try {
-                console.log(`Fetching cards for board ${board.boardId}`);
                 const cards = await fetchData(`http://localhost:8080/api/boards/${board.boardId}/cards`);
 
                 for (const card of cards) {
@@ -27,19 +106,4 @@ async function fetchBoardsAndCards() {
     }
 }
 
-function createBoardDiv(board) {
-    console.log(board);
-    const div = document.createElement('div');
-    div.className = 'board';
-
-    // Convert columns object into a string
-    let columnsString = '';
-    for (let key in board.columns) {
-        columnsString += `Column ${key}: ${board.columns[key]}, `;
-    }
-    // Remove last comma and space
-    columnsString = columnsString.slice(0, -2);
-
-    div.textContent = `${board.title} - ${columnsString}`;
-    return div;
-}
+fetchBoardsAndCards();
